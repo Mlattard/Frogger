3efd4398203884d60884969da1831acf58183b13
Mlattard <maxime@lattard.fr>
2023-03-24 21:54:48 -0400

    Display background and car animation

--- code/main.py
+++ code/main.py
@@ -2,6 +2,23 @@ import pygame, sys
 from random import randint, uniform
 from settings import *
 from player import Player
+from car import Car
+
+class AllSprites(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.bg = pygame.image.load('graphics/main/map.png').convert()
+        self.fg = pygame.image.load('graphics/main/overlay.png').convert_alpha()
+
+    def customize_draw(self):
+
+        display_surface.blit(self.bg, (0, 0))
+        display_surface.blit(self.fg, (0, 0))         
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft + self.offset
+            display_surface.blit(sprite.image, offset_pos)
 
 # basic setup
 pygame.init()
@@ -10,10 +27,11 @@ pygame.display.set_caption("Frogger-like")
 clock = pygame.time.Clock()
 
 # groups
-all_sprites = pygame.sprite.Group()
+all_sprites = AllSprites()
 
 # sprites
-player = Player((600, 400), all_sprites)
+player = Player((WINDOW_WIDTH  /2, WINDOW_HEIGHT /2), all_sprites)
+car = Car((100, 200), all_sprites)
 
 # game loop
 while True:
@@ -34,7 +52,7 @@ while True:
     all_sprites.update(dt)
 
     # draw
-    all_sprites.draw(display_surface)
+    all_sprites.customize_draw()
 
     # update the display surface
     pygame.display.update()
